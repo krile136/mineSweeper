12a7b712d0f55f0aa577aca697c57ebe6f22b214
krile136 <krile136cm@gmail.com>
2022-04-28 12:49:54 +0900

    画像の更新とメソッド引数の整理

--- scenes/minesweeper/draw.go
+++ scenes/minesweeper/draw.go
@@ -6,6 +6,7 @@ import (
 )
 
 func (m *MineSweeper) Draw(screen *ebiten.Image) {
+	// 各ブロックを表示
 	c := 1.0
 	p := 32
 	for i := 0; i < m.rows; i++ {
@@ -14,31 +15,32 @@ func (m *MineSweeper) Draw(screen *ebiten.Image) {
 			y := (float64(i)+0.5)*float64(p)*c + scrollY
 			switch m.field[i][j] {
 			case close:
-				draw.Draw(screen, "minesweeper", c, x, y, 0, 0, 0, p, p)
+				draw.Draw(screen, "minesweeper", c, c, x, y, 0, 0, 0, p, p)
 			case zero:
-				draw.Draw(screen, "minesweeper", c, x, y, 0, p, 0, p, p)
+				draw.Draw(screen, "minesweeper", c, c, x, y, 0, p, 0, p, p)
 			case flag:
-				draw.Draw(screen, "minesweeper", c, x, y, 0, p*2, 0, p, p)
+				draw.Draw(screen, "minesweeper", c, c, x, y, 0, p*2, 0, p, p)
 			case bomb:
-				draw.Draw(screen, "minesweeper", c, x, y, 0, p*3, 0, p, p)
+				draw.Draw(screen, "minesweeper", c, c, x, y, 0, p*3, 0, p, p)
 			case one:
-				draw.Draw(screen, "minesweeper", c, x, y, 0, 0, p, p, p)
+				draw.Draw(screen, "minesweeper", c, c, x, y, 0, 0, p, p, p)
 			case two:
-				draw.Draw(screen, "minesweeper", c, x, y, 0, p, p, p, p)
+				draw.Draw(screen, "minesweeper", c, c, x, y, 0, p, p, p, p)
 			case three:
-				draw.Draw(screen, "minesweeper", c, x, y, 0, p*2, p, p, p)
+				draw.Draw(screen, "minesweeper", c, c, x, y, 0, p*2, p, p, p)
 			case four:
-				draw.Draw(screen, "minesweeper", c, x, y, 0, p*3, p, p, p)
+				draw.Draw(screen, "minesweeper", c, c, x, y, 0, p*3, p, p, p)
 			case five:
-				draw.Draw(screen, "minesweeper", c, x, y, 0, p*4, p, p, p)
+				draw.Draw(screen, "minesweeper", c, c, x, y, 0, p*4, p, p, p)
 			case six:
-				draw.Draw(screen, "minesweeper", c, x, y, 0, p*5, p, p, p)
+				draw.Draw(screen, "minesweeper", c, c, x, y, 0, p*5, p, p, p)
 			case seven:
-				draw.Draw(screen, "minesweeper", c, x, y, 0, p*6, p, p, p)
+				draw.Draw(screen, "minesweeper", c, c, x, y, 0, p*6, p, p, p)
 			case eight:
-				draw.Draw(screen, "minesweeper", c, x, y, 0, p*7, p, p, p)
+				draw.Draw(screen, "minesweeper", c, c, x, y, 0, p*7, p, p, p)
 			}
 		}
 	}
 
+	// スクロールバーを表示
 }
